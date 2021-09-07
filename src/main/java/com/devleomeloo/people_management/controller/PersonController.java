1ce1741235f4dda4dcca7bcf0baa51e9dedff5b0
devleomeloo <leomelojesus@gmail.com>
2021-09-07 19:57:32 -0300

    refactoring

--- src/main/java/com/devleomeloo/people_management/controller/PersonController.java
+++ src/main/java/com/devleomeloo/people_management/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.devleomeloo.people_management.dto.MessageResponseDTO;
 import com.devleomeloo.people_management.dto.request.PersonDTO;
 import com.devleomeloo.people_management.exception.PersonNotFoundException;
 import com.devleomeloo.people_management.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
