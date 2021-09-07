8957fc01b5acc6a5bc764a2769e84b4432cc1f95
Leonardo Melo <leomelojesus@gmail.com>
2021-09-06 22:00:39 -0300

    personService created

--- src/main/java/com/devleomeloo/people_management/controller/PersonController.java
+++ src/main/java/com/devleomeloo/people_management/controller/PersonController.java
@@ -2,27 +2,25 @@ package com.devleomeloo.people_management.controller;
 
 import com.devleomeloo.people_management.dto.MessageResponseDTO;
 import com.devleomeloo.people_management.entity.Person;
-import com.devleomeloo.people_management.repository.PersonRepository;
+import com.devleomeloo.people_management.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/corinthians")
+@RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with DTO")
-                .build();
+        return personService.createPerson(person);
     }
 }
---
+++ src/main/java/com/devleomeloo/people_management/service/PersonService.java
@@ -0,0 +1,28 @@
+package com.devleomeloo.people_management.service;
+
+import com.devleomeloo.people_management.dto.MessageResponseDTO;
+import com.devleomeloo.people_management.entity.Person;
+import com.devleomeloo.people_management.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
